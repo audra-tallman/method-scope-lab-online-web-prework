509a4141aeba7a612951bde71ff8252bb6f56786
Audra Tallman <audra.tallman@hotmail.com>
2019-02-20 15:45:40 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -4,9 +4,5 @@ def rescue_princess_peach(status)
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-
-
-def rescue_princess_peach(status)
-  puts "#{status}"
-end
-
+#rescue_princess_peach
+#puts status
